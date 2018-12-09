fec404123802dde164b0b1056afb0474ca7facd1
jonathan gerber <jlgerber@gmail.com>
2018-12-08 16:13:50 -0800

    add shotgun test

---
+++ src/tests/shotgundb.py
@@ -0,0 +1,85 @@
+import unittest
+import os
+import sys
+pth = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(pth)
+from fauxit.model.shotgundb import FauxLevel, Shot, Sequence
+
+
+class FauxLevelP(FauxLevel):
+
+    def __init__(self, name):
+        super(FauxLevelP,self).__init__(name)
+FauxLevelP.child_cls = FauxLevelP
+
+class FauxLevelTest(unittest.TestCase):
+    """testing FauxLevel"""
+    def test_has_child(self):
+        fl = FauxLevelP("FOO")
+        fl.add_child("BAR")
+        self.assertTrue(fl.has_child("BAR"))
+        self.assertFalse(fl.has_child("DONT"))
+
+    def test_add_child(self):
+        fl = FauxLevelP("FOO")
+        fl.add_child("BAR")
+        self.assertEqual(len(fl.children),1)
+        self.assertTrue(fl.has_child("BAR"))
+
+    def test_remove_child(self):
+        fl = FauxLevelP("FOO")
+        fl.add_child("BAR")
+        child = fl.remove_child("BAR")
+        self.assertEqual(len(fl.children), 0)
+        self.assertEqual(child.name, "BAR")
+
+    def test_remove_child_nonext(self):
+        fl = FauxLevelP("FOO")
+        fl.add_child("BAR")
+        child = fl.remove_child("BLA")
+        self.assertEqual(len(fl.children), 1)
+        self.assertEqual(child, None)
+
+    def test_child(self):
+        fl = FauxLevelP("FOO")
+        fl.add_child("BAR")
+        self.assertEquals(len(fl.children), 1)
+        child = fl.child("BAR")
+        self.assertEquals(child.name, "BAR")
+        self.assertEquals(len(fl.children), 1)
+
+
+class ShotTest(unittest.TestCase):
+    def test_init(self):
+        shot = Shot("0001")
+        self.assertEquals(shot.name, "0001")
+
+    def test_str(self):
+        shot = Shot("0001")
+        self.assertEquals(str(shot), "0001")
+
+class SequenceTest(unittest.TestCase):
+    def test_init(self):
+        seq = Sequence("RD")
+        self.assertEquals(seq.name, "RD")
+        self.assertEquals(str(seq), "RD")
+
+    def test_add_shot(self):
+        seq = Sequence("RD")
+        self.assertEqual(len(seq.children), 0)
+
+        seq.add_shot("0001")
+        self.assertEqual(len(seq.children), 1)
+        self.assertTrue(seq.has_child("0001"))
+
+    def test_remove_shot(self):
+        seq = Sequence("RD")
+        self.assertEqual(len(seq.children), 0)
+        seq.add_shot("0001")
+        self.assertEqual(len(seq.children), 1)
+        shot = seq.remove_shot("0001")
+        self.assertEqual(len(seq.children),0)
+        self.assertEquals(shot.name, "0001")
+
+if __name__ == '__main__':
+    unittest.main()
