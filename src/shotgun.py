f16204dd5bc595ae5966edf772228f00cc4da05e
jonathan gerber <jlgerber@gmail.com>
2018-12-08 16:13:25 -0800

    adding differntiation between http and rpc

--- src/shotgun.py
+++ src/shotgun.py
@@ -1,4 +1,7 @@
+import json
 from nameko.rpc import rpc
+from nameko.web.handlers import http
+
 from fauxit.model.shotgundb import SGDB
 from fauxit.model.levelspec import LevelSpec
 
@@ -22,15 +25,29 @@ class ShotgunService(object):
             raise KeyError("project: {} does not exist in shotgun".format(project))
         SHOTGUN.project(project).add_asset(name)
 
+    def _assets(self,project):
+        return SHOTGUN.project(project).assets
+
     @rpc
     def assets(self, project):
-        return SHOTGUN.project(project).assets
+        return self._assets(project)
+
+    @http('GET', '/sgassets/<string:project>')
+    def get_assets_http(self, request, project):
+        assets = self._assets(project)
+        return json.dumps({ 'assets': assets})
 
     def _projects(self):
         return SHOTGUN.children.keys()
+
     @rpc
     def projects(self):
         return self._projects()
+
+    @http('GET', '/sgprojects')
+    def get_projects_http(self, request):
+        projects= self._projects()
+        return json.dumps({ 'projects': projects})
 
     @rpc
     def child_levels(self, level=None):
